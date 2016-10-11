ff2100446e22d10f6cad263634628df03b443228
Jack Baron <yodude107@gmail.com>
2016-10-11 18:46:18 +0100

    Better Tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const open = require('open')
 const RetroText = require(path.join("..", "main.js"))
 const text = new RetroText()
 
@@ -10,4 +11,4 @@ text.generate({
     line2: "Line 2",
     line3: "Line 3"
   }
-}).then(url => { console.log(url); }).catch( console.log );
+}).then(url => { open(url); }).catch( console.log );
