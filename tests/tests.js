f9833717c9551f45f77d80e4f9370d4cf1b57791
Jack Baron <yodude107@gmail.com>
2016-10-12 19:33:34 +0100

    Unit Testing

--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,9 @@
-const path = require('path')
-const open = require('open')
+const path      = require('path')
+const open      = require('open')
+const r         = require('request')
+const crypto    = require('crypto');
 const RetroText = require(path.join("..", "main.js"))
-const text = new RetroText()
+const text      = new RetroText()
 
 text.generate({
   background: 3,
@@ -11,4 +13,23 @@ text.generate({
     line2: "Line 2",
     line3: "Line 3"
   }
-}).then(url => { open(url); process.exit(1); }).catch( err => { console.log; process.exit(1); throw err;  } );
+}).then(url => {
+    r.get(url, (error, request, body) => {
+      if (!error) {
+        // Test Image Hash
+        let hash = crypto.createHash('md5').update(body).digest('hex');
+        if (hash === "92a440c856d5f9150c1d527a9271729d") {
+          // Hash is good
+          process.exit(0)
+        } else {
+          // Hash is wrong
+          process.exit(1)
+        }
+      } else {
+        process.exit(1)
+      }
+    })
+}).catch( err => {
+  // Failure  
+  process.exit(1)
+});
