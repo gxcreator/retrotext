5db2abe3244fbd8e35e64d1994b178ce184b682f
Jack Baron <yodude107@gmail.com>
2016-11-03 16:50:31 +0000

    Lint Test

--- test/test.js
+++ test/test.js
@@ -9,7 +9,6 @@
 
 // Require Dependencies
 const path      = require('path')
-const open      = require('open')
 const r         = require('request')
 const crypto    = require('crypto')
 
@@ -22,17 +21,17 @@ text.generate({
   background: 3,
   textStyle: 2,
   text: {
-    line1: "Line 1",
-    line2: "Line 2",
-    line3: "Line 3"
-  }
+    line1: 'Line 1',
+    line2: 'Line 2',
+    line3: 'Line 3',
+  },
 }).then(url => {
   // Check the test result
   r.get(url, (error, request, body) => {
     if (!error) {
       // Test Image Hash
       let hash = crypto.createHash('md5').update(body).digest('hex')
-      if (hash === "92a440c856d5f9150c1d527a9271729d") {
+      if (hash === '92a440c856d5f9150c1d527a9271729d') {
         // Hash is good
         process.exit(0)
       } else {
@@ -43,7 +42,7 @@ text.generate({
       process.exit(1)
     }
   })
-}).catch( err => {
-  // Failure  
+}).catch(() => {
+  // Failure
   process.exit(1)
 })
