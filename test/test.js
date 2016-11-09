9fe9cf4d4f207659a880389a47483fe227f9147f
Jack Baron <yodude107@gmail.com>
2016-11-09 11:23:18 +0000

    Restructure to class based system

--- test/test.js
+++ test/test.js
@@ -13,35 +13,12 @@ const r         = require('request')
 const crypto    = require('crypto')
 
 // Require Self
-const RetroText = require(path.join('..', 'lib', '/'))
-const text      = new RetroText()
-
-// Test it
-text.generate([
+const RetroText = require(path.join('..', 'lib'))
+const text      = new RetroText([
   'Line 1',
   'Line 2',
   'Line 3',
 ], {
   background: 3,
   textStyle: 2,
-}).then(url => {
-  // Check the test result
-  r.get(url, (error, request, body) => {
-    if (!error) {
-      // Test Image Hash
-      let hash = crypto.createHash('md5').update(body).digest('hex')
-      if (hash === '92a440c856d5f9150c1d527a9271729d') {
-        // Hash is good
-        process.exit(0)
-      } else {
-        // Hash is wrong
-        process.exit(1)
-      }
-    } else {
-      process.exit(1)
-    }
-  })
-}).catch(() => {
-  // Failure
-  process.exit(1)
 })
